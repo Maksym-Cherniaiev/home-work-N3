8ee4e3d3d99aed9435f1acf1cf78219922c2405f
Maksym <likeran2@gmail.com>
2019-02-21 15:33:20 +0200

    added alarm section

--- js/time.js
+++ js/time.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     (function(){
         let date = new Date();
-        let time = date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+        let time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
         document.querySelector(".get-time").textContent = time;
         window.setTimeout(arguments.callee, 1000);
     })();
